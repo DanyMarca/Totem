efb16bbe08839fc599b5a4295b9138e95bbfd7b3
DanieleLaMarca <71449152+xLordPvP@users.noreply.github.com>
2025-01-20 10:21:08 +0100

    aggiunta-file-bat

---
+++ Totem.bat
@@ -0,0 +1,38 @@
+@echo off
+echo Avvio del servizio MySQL...
+start /b "MySQL" "C:\xampp\mysql\bin\mysqld"
+if %errorlevel% neq 0 (
+    echo Errore nell'avvio del servizio MySQL. Controllare il percorso.
+    pause
+    exit /b
+)
+
+echo Avvio del server Laravel...
+cd C:\Users\pc\Desktop\lavoro\Totem\BE
+start /b "Laravel" cmd /c "php artisan serve"
+if %errorlevel% neq 0 (
+    echo Errore nell'avvio del server Laravel.
+    pause
+    exit /b
+)
+
+echo Avvio del server Angular...
+cd C:\Users\pc\Desktop\lavoro\Totem\FE
+start /b "Angular" cmd /c "ng serve"
+if %errorlevel% neq 0 (
+    echo Errore nell'avvio del server Angular.
+    pause
+    exit /b
+)
+
+echo Apertura del browser su Google...
+start chrome --start-fullscreen "http://localhost:4200/"
+if %errorlevel% neq 0 (
+    echo Errore nell'apertura del browser.
+    pause
+    exit /b
+)
+
+echo Tutti i servizi sono stati avviati e la pagina web è stata aperta.
+pause
+exit
