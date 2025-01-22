46e64abfe771fc99aeee9833da5868b60db2bdb0
DanieleLaMarca <71449152+xLordPvP@users.noreply.github.com>
2025-01-22 08:19:45 +0100

    totembat-sistemato

--- Totem.bat
+++ Totem.bat
@@ -1,4 +1,5 @@
 @echo off
+
 echo Avvio del servizio MySQL...
 start /b "MySQL" "C:\xampp\mysql\bin\mysqld"
 if %errorlevel% neq 0 (
@@ -7,8 +8,11 @@ if %errorlevel% neq 0 (
     exit /b
 )
 
+:: Ottieni il percorso della directory dello script attuale
+set SCRIPT_DIR=%~dp0
+
 echo Avvio del server Laravel...
-cd C:\Users\pc\Desktop\lavoro\Totem\BE
+cd "%SCRIPT_DIR%BE"
 start /b "Laravel" cmd /c "php artisan serve"
 if %errorlevel% neq 0 (
     echo Errore nell'avvio del server Laravel.
@@ -17,7 +21,7 @@ if %errorlevel% neq 0 (
 )
 
 echo Avvio del server Angular...
-cd C:\Users\pc\Desktop\lavoro\Totem\FE
+cd "%SCRIPT_DIR%FE"
 start /b "Angular" cmd /c "ng serve"
 if %errorlevel% neq 0 (
     echo Errore nell'avvio del server Angular.
