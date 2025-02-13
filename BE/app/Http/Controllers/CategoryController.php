db9385f8d0ea4ec959019316ebf05affc66d5397
DanieleLaMarca <71449152+xLordPvP@users.noreply.github.com>
2025-02-13 11:33:27 +0100

    immagini-verticali-show

--- BE/app/Http/Controllers/CategoryController.php
+++ BE/app/Http/Controllers/CategoryController.php
@@ -13,25 +13,46 @@ class CategoryController extends Controller
         return $serverIp . $path;
     }
 
-    private function findImage($obj)
-    {
-        $images = $obj->filestorageable()
-            ->select('path', 'orientation')
-            ->whereIn('orientation', ['horizontal', 'vertical'])
-            ->get()
-            ->keyBy('orientation');
+    private function findImage($obj, $filter = null)
+{
+    // Recupera le immagini con orientamento 'horizontal' e 'vertical'
+    $images = $obj->filestorageable()
+        ->select('path', 'orientation')
+        ->whereIn('orientation', ['horizontal', 'vertical'])
+        ->get()
+        ->keyBy('orientation');
 
+    // Se è presente un filtro, restituisci solo l'immagine con quell'orientamento
+    if ($filter) {
+        // Verifica se l'orientamento richiesto esiste nelle immagini recuperate
+        if (isset($images[$filter])) {
+            return [
+                $filter => (object) [
+                    'path' => $this->getFullPath($images->get($filter)->path),
+                    'orientation' => $filter,
+                ]
+            ];
+        }
+
+        // Se non viene trovata un'immagine con l'orientamento specificato, restituisci null
         return [
-            'horizontal' => $images->get('horizontal') ? (object) [
-                'path' => $this->getFullPath($images->get('horizontal')->path),
-                'orientation' => 'horizontal'
-            ] : null,
-            'vertical' => $images->get('vertical') ? (object) [
-                'path' => $this->getFullPath($images->get('vertical')->path),
-                'orientation' => 'vertical'
-            ] : null,
+            $filter => null
         ];
     }
+
+    // Se non c'è filtro, restituisci entrambe le immagini (orizzontale e verticale)
+    return [
+        'horizontal' => $images->get('horizontal') ? (object) [
+            'path' => $this->getFullPath($images->get('horizontal')->path),
+            'orientation' => 'horizontal'
+        ] : null,
+        'vertical' => $images->get('vertical') ? (object) [
+            'path' => $this->getFullPath($images->get('vertical')->path),
+            'orientation' => 'vertical'
+        ] : null,
+    ];
+}
+
 
     public function index()
     {
@@ -43,25 +64,13 @@ class CategoryController extends Controller
 
         foreach ($categories as $category) {
             $images = $this->findImage($category);
+            $category->image = array_values(array_filter($images));
 
-            // Mantiene solo un'immagine orizzontale e una verticale
-            $filteredImages = array_filter([
-                'horizontal' => $images['horizontal'],
-                'vertical' => $images['vertical']
-            ]);
-
-            // Assegna solo le immagini disponibili
-            $category->image = array_values($filteredImages);
-
-            $type = $category->type === 'liceo' ? 'Liceo' : 'Tecnico';
-
-            // Aggiunge al carosello solo se c'è un'immagine orizzontale
             if ($images['horizontal']) {
+                $type = $category->type === 'liceo' ? 'Liceo' : 'Tecnico';
                 $data[$type]['carosello'][] = $images['horizontal']->path;
+                $data[$type]['categories'][] = $category;
             }
-
-            // Aggiunge sempre la categoria
-            $data[$type]['categories'][] = $category;
         }
 
         return response()->json([
@@ -73,10 +82,11 @@ class CategoryController extends Controller
     public function show($id)
     {
         $category = Category::findOrFail($id);
-        $category->image = array_values(array_filter($this->findImage($category)));
-
+        $images = $this->findImage($category, "vertical");
+        $category->image = array_values(array_filter($images));
+        
         $category->laboratories->each(function ($lab) {
-            $lab->image = array_values(array_filter($this->findImage($lab)));
+            $lab->image = $this->findImage($lab);
         });
 
         return response()->json([
